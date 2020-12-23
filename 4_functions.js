184bbfa7667f6ee5d2d21803070c4bc867c99619
Dani <danifdezalonso@gmail.com>
2020-12-23 15:41:06 +0100

    5_objectsMaps

--- 4_functions.js
+++ 4_functions.js
@@ -46,3 +46,82 @@ console.log(suma5(5)); // resultado 10 (recuerda el primer valor de la linea ant
 
 let suma10 = makeSum(10); 
 console.log(suma10(10)); // resultado 20 (recuerda el 10 de antes y suma este)
+
+// Challenge: Write a countdown function with a hard-coded starting number inside closure
+// Stretch goal: Write a countdown function that can count from a provided number,
+// with a provided step
+
+// Start
+function countdown(startingNumber, step) {
+  let countFromNum = startingNumber + step;
+  return function decrease() {
+    countFromNum -= step;
+    return countFromNum;
+  }
+}
+
+const countingDown = countdown(20, 5);
+console.log(countingDown());
+console.log(countingDown());
+console.log(countingDown());
+
+
+// ------------- BETTER FUNCTIONS WITH DEFAULT PARAMETERS
+function convertTemperature(celsius, decimalPlaces = 1) {  //decimalPlaces = 1 --> indica que si no se añade segundo parametro
+  // celsius to fahrenheit                                   al llamar la funcion, por defecto será 1
+const fahrenheit = celsius * 1.8 + 32;
+return Number(fahrenheit.toFixed(decimalPlaces));
+}
+console.log(convertTemperature(21)); // result 69, 8 (por defecto 1 decimal)
+console.log(convertTemperature(21, 0)); // result: 70
+
+//--------------- ARROW FUNCTIONS =>
+// Arrow functions allow us to write more concise code. They are:
+// function expressions (assigned to a variable)
+// anonymous (can't give them a name)
+// Implicit returns mean that the value following the => is always returned, so the return keyword is not needed.
+
+const username = 'john';
+
+function capitalizeName(name) { //mayuscula primera letra nombre
+  return `${name.charAt(0).toUpperCase()}${name.slice(1)}`;  
+}
+
+// convertir funcion a arrow, se le asigna a una variable
+const capitalize = function capitalizeName(name) { 
+  return `${name.charAt(0).toUpperCase()}${name.slice(1)}`;  
+}
+
+//se hace anonima (quitar el nombre de la funcion)
+const capitalize = function(name) {
+  return `${name.charAt(0).toUpperCase()}${name.slice(1)}`;  
+}
+
+// quitar la keyword function
+const capitalize = (name) { 
+  return `${name.charAt(0).toUpperCase()}${name.slice(1)}`;  
+}
+
+// añadir la fecha apuntando al body de la funcion
+const capitalize = (name) => { 
+  return `${name.charAt(0).toUpperCase()}${name.slice(1)}`;  
+}
+
+// ---> si *solo* tenemos un parametro se pueden eliminar los parentensis
+const capitalize = name => { 
+  return `${name.charAt(0).toUpperCase()}${name.slice(1)}`;  
+}
+
+// ---> con dos parametros se ponen los parentesis y coma como siempre
+const capitalize = (name, age) => { 
+  return `${name.charAt(0).toUpperCase()}${name.slice(1)}`;  
+}
+
+// return implicito en arrow functions, no necesita return ni los brackets
+const capitalize = name => 
+  `${name.charAt(0).toUpperCase()}${name.slice(1)}`;  
+
+// todo en la misma linea
+const capitalize = name => `${name.charAt(0).toUpperCase()}${name.slice(1)}`;  
+
+//------------ FUNCTIONS NAME => VERBS
